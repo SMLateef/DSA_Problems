d92798c54399fae0e008ce41a4bb49e32c6ebcb7
SMLateef <138566573+SMLateef@users.noreply.github.com>
2023-07-15 10:55:23 +0530

    Add files via upload

---
+++ Natural_number.py
@@ -0,0 +1,3 @@
+num = int(input("Enter the number to find natural num :- "))
+for i in range(1,num + 1):
+    print(i,end = " ")
---
+++ Whole_numbers.py
@@ -0,0 +1,3 @@
+num = int(input("Enter the number to find whole num :- "))
+for i in range(0,num + 1):
+    print(i,end = " ")
